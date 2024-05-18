f0e004cb4e59dbec33a3bcaf64e2d261a331549a
CristianN01 <cristiannechifor71@gmail.com>
2024-05-18 12:49:12 +0200

    selezionato il mio container con js e creato un ciclo for, per la creazione delle mie cell dentro al container con js

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,11 @@
 const gridContainer = document.querySelector('section.container')
 
+// ? <article class="cell"></article>
+
+for (let index = 0; index < 64; index++) {
+
+    const cellElement = document.createElement('article');
+    cellElement.classList.add('cell')
+
+    gridContainer.appendChild(cellElement)
+}
