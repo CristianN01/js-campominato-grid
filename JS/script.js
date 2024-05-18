cc287f0c4f2bebc517beeac5214a0003071961b0
CristianN01 <cristiannechifor71@gmail.com>
2024-05-18 13:03:05 +0200

    creato un evento con addeventlistner di tipo ckick, cambiando il colore della cella al suo click su di essa, aggiunto numeri dentro le celle con append, e inserito index allinterno che vada da 1 a 64 aggiungendo + 1 dentro alle parentesi imnsieme a index, ed infine modificato colore e posizione tramite css

--- JS/script.js
+++ JS/script.js
@@ -6,6 +6,10 @@ for (let index = 0; index < 64; index++) {
 
     const cellElement = document.createElement('article');
     cellElement.classList.add('cell')
+    cellElement.append(index + 1)
+    cellElement.addEventListener('click', function(){
+        cellElement.classList.add('active')
+    });
 
     gridContainer.appendChild(cellElement)
 }
