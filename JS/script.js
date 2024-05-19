64abdf2d50fbaf9c252a5fbf7df161cb7f927802
CristianN01 <cristiannechifor71@gmail.com>
2024-05-19 23:40:16 +0200

    risolto pulsante play, facendo scomparire la griglia e ricreandola con il ciclo for all'interno cosi da non dover riomuovo la classe active per far sembrare che si sia rigenerata la griglia

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 const gridContainer = document.querySelector('section.container')
-
 const playButton = document.querySelector('button.play')
+
 
 
 // ? <article class="cell"></article>
@@ -15,7 +15,21 @@ for (let index = 0; index < 64; index++) {
     });
 
     playButton.addEventListener('click', function(){
-        cellElement.classList.remove('active')
+        // cellElement.classList.remove('active')
+      document.querySelector('section.container').innerHTML = "";
+      
+      for (let index = 0; index < 64; index++) {
+
+        const cellElement = document.createElement('article');
+        cellElement.classList.add('cell')
+        cellElement.append(index + 1)
+        cellElement.addEventListener('click', function(){
+            cellElement.classList.toggle('active')
+        });
+    
+    
+        gridContainer.appendChild(cellElement)
+    }
     });
 
 
