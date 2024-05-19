b89cc73cfde5f09ee4963e96eae1c6be46df496d
CristianN01 <cristiannechifor71@gmail.com>
2024-05-20 00:53:51 +0200

    sistemato bottone play e aggiunto scritta iniziale pagina

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,13 @@
 const gridContainer = document.querySelector('section.container')
 const playButton = document.querySelector('button.play')
 
-
-
 // ? <article class="cell"></article>
 
-for (let index = 0; index < 64; index++) {
+
+playButton.addEventListener('click', function(){
+    document.querySelector('section.container').innerHTML = "";
+    
+    for (let index = 0; index < 64; index++) {
 
     const cellElement = document.createElement('article');
     cellElement.classList.add('cell')
@@ -14,24 +16,15 @@ for (let index = 0; index < 64; index++) {
         cellElement.classList.toggle('active')
     });
 
-    playButton.addEventListener('click', function(){
-        // cellElement.classList.remove('active')
-      document.querySelector('section.container').innerHTML = "";
-      
-      for (let index = 0; index < 64; index++) {
-
-        const cellElement = document.createElement('article');
-        cellElement.classList.add('cell')
-        cellElement.append(index + 1)
-        cellElement.addEventListener('click', function(){
-            cellElement.classList.toggle('active')
-        });
-    
-    
-        gridContainer.appendChild(cellElement)
-    }
-    });
-
 
     gridContainer.appendChild(cellElement)
 }
+});
+
+
+
+    
+
+
+
+   
